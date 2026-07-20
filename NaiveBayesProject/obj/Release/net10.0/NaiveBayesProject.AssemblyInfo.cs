bb9d7035ed9e00c8ec7ab46c1277485c12cdc54d
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-07-20 12:47:44 +0300

    readme file

--- NaiveBayesProject/obj/Release/net10.0/NaiveBayesProject.AssemblyInfo.cs
+++ NaiveBayesProject/obj/Release/net10.0/NaiveBayesProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("NaiveBayesProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127f07f07bf00409a761181fff3481a95e14be67")]
 [assembly: System.Reflection.AssemblyProductAttribute("NaiveBayesProject")]
